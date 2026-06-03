7405eaf9569211e79e07db0b19ba9c3d0b3f488e
Vicktor <vitorlx456re2cod@gmail.com>
2026-06-03 10:42:53 -0400

    feat: adiciona função de soma

---
+++ bomdia.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Cumprimento {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Digite o seu nome: ");
+        String nome = scanner.nextLine();
+        System.out.println("Bom dia, " + nome + "!");
+        scanner.close();
+    }
+}
